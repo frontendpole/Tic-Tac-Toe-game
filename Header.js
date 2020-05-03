83d6dec76c8049430096915b12df28654ece21c2
Ola <aleksandra.karp1@gmail.com>
2020-05-03 17:02:56 +0200

    all files added

---
+++ Header.js
@@ -0,0 +1,19 @@
+export default class Header {
+    constructor(header, text) {
+        this.header = this.createHeader();
+        this.text = this.addText();
+    }
+
+    createHeader() {
+        const h1 = document.createElement('h1');
+        document.body.appendChild(h1);
+        h1.classList.add('welcome');
+    }
+
+    addText() {
+        document.querySelector('h1').textContent = 'Welcome to tic-tac-toe!';
+    }
+}
+
+
+
